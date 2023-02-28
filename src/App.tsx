a6522f1f44ec490a9da6fa441a708e33748317a0
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-28 17:58:05 -0300

    Add routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
 import { GlobalStyles } from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/defaultTheme'
@@ -5,6 +8,10 @@ import { defaultTheme } from './styles/theme/defaultTheme'
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyles />
     </ThemeProvider>
   )
---
+++ src/pages/Post/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export function Post() {
+  return (
+    <S.Container>
+      <h1>Post</h1>
+    </S.Container>
+  )
+}
