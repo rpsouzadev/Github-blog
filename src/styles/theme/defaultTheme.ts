1cdc506058ebbdcbfd398dd9489f2d6f407de33e
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-02 23:41:45 -0300

    Add fonts size in theme

--- src/styles/theme/defaultTheme.ts
+++ src/styles/theme/defaultTheme.ts
@@ -12,4 +12,12 @@ export const defaultTheme = {
     'base-title': '#E7EDF4',
     'brand-blue': '#3294F8',
   },
+  textSizes: {
+    'title-title-l': '1.5rem',
+    'title-title-m': '1.25rem',
+    'title-title-s': '1.125rem',
+    'text-text-m': '1rem',
+    'text-text-s': '0.875rem',
+    'components-link': '0.75rem',
+  },
 } as const
