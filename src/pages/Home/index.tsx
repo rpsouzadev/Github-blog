a157ca66f484a9607344c237712412c1c43e9d6c
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-10 20:31:35 -0300

    Add api with axios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import { PostsComponent } from './components/PostsComponent'
 import { Profile } from './components/Profile'
 import { SearchForm } from './components/SearchForm'
 import * as S from './styles'
 
+interface PostsProps {
+  title: string
+}
+
 export function Home() {
+  const [posts, setPosts] = useState<PostsProps>([])
+
   return (
     <>
       <Profile />
