1fdd5e9e3ffe386fe9435cbafdd02c4434ff8907
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-14 11:22:04 -0300

    Add search for posts in the api

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,13 +1,35 @@
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import * as S from './styles'
 
-export function SearchForm() {
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormSchemaProps = z.infer<typeof searchFormSchema>
+
+interface SearchFormProps {
+  getPost: (query?: string) => Promise<void>
+}
+
+export function SearchForm({ getPost }: SearchFormProps) {
+  const { register, handleSubmit } = useForm<SearchFormSchemaProps>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchPosts(data: SearchFormSchemaProps) {
+    await getPost(data.query)
+  }
+
   return (
-    <S.SearchContainer>
+    <S.SearchContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <header>
         <h3>Publicações</h3>
         <span>6 pubicações</span>
       </header>
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input type="text" placeholder="Buscar conteúdo" {...register('query')} />
     </S.SearchContainer>
   )
 }
--- src/pages/Home/components/SearchForm/styles.ts
+++ src/pages/Home/components/SearchForm/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const SearchContainer = styled.div`
+export const SearchContainer = styled.form`
   width: 100%;
   margin-top: 4.5rem;
   margin-bottom: 3rem;
