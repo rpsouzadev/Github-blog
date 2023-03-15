7558a2f0170ab5c1329508938a43a6289873faa0
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-14 23:14:34 -0300

    Made the integration of the api with the profile

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,38 +1,73 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useCallback, useEffect, useState } from 'react'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { LinkComponent } from '../../../../components/LinkComponent'
 import { faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'
 
 import * as S from './styles'
-import { LinkComponent } from '../../../../components/LinkComponent'
+import { api } from '../../../../lib/axios'
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+
+interface ProfileData {
+  login: string
+  bio: string
+  avatar_url: string
+  html_url: string
+  name: string
+  company?: string
+  followers: number
+}
 
 export function Profile() {
+  const [profileData, setProfileData] = useState<ProfileData>({} as ProfileData)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getProfileData = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const response = await api.get(`/users/${username}`)
+
+      setProfileData(response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    getProfileData()
+  }, [getProfileData])
+
   return (
     <S.ProfileContainer>
-      <S.ProfileAvatar src="https://github.com/Muglly.png" />
+      <S.ProfileAvatar src={profileData.avatar_url} />
 
       <S.ProfileContent>
         <header>
-          <h1>Rafael Souza</h1>
+          <h1>{profileData.name}</h1>
 
-          <LinkComponent text="github" href="#" />
+          <LinkComponent
+            text="github"
+            href={profileData.html_url}
+            target="_blank"
+          />
         </header>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, vitae
-          inventore? Incidunt!
-        </p>
+        <p>{profileData.bio}</p>
         <ul>
           <li>
             <FontAwesomeIcon icon={faGithub} />
-            Muglly
+            {profileData.login}
           </li>
 
-          <li>
-            <FontAwesomeIcon icon={faBuilding} />
-            Autonômo
-          </li>
+          {profileData?.company && (
+            <li>
+              <FontAwesomeIcon icon={faBuilding} />
+              {profileData.company}
+            </li>
+          )}
           <li>
             <FontAwesomeIcon icon={faUserGroup} />
-            seguidores
+            {profileData.followers} seguidores
           </li>
         </ul>
       </S.ProfileContent>
