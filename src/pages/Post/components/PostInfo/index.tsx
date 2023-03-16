f47949620ab4eea3d21859812b285e84d9f1d18e
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-03-16 11:17:58 -0300

    Add integration with PostInfo

--- src/pages/Post/components/PostInfo/index.tsx
+++ src/pages/Post/components/PostInfo/index.tsx
@@ -5,49 +5,68 @@ import {
   faComment,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 import { LinkComponent } from '../../../../components/LinkComponent'
-
 import { useNavigate } from 'react-router-dom'
+import { PostsProps } from '../../../Home'
 
 import * as S from './styles'
+import { Spinner } from '../../../../components/Spinner'
+import { relativeDateFormatter } from '../../../../utils/formatter'
 
-export function PostInfo() {
+interface PostInfoProps {
+  postData: PostsProps
+  isLoading: boolean
+}
+
+export function PostInfo({ postData, isLoading }: PostInfoProps) {
   const navigate = useNavigate()
 
   function goBack() {
     navigate(-1)
   }
 
+  const formattedDate = relativeDateFormatter(postData?.created_at)
+
   return (
     <S.PostInfoContainer>
-      <header>
-        <LinkComponent
-          as="button"
-          icon={<FontAwesomeIcon icon={faChevronLeft} />}
-          variant="iconLeft"
-          text="voltar"
-          onClick={goBack}
-        />
-        <LinkComponent text="ver no github" href="#" target="_blank" />
-      </header>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <header>
+            <LinkComponent
+              as="button"
+              icon={<FontAwesomeIcon icon={faChevronLeft} />}
+              variant="iconLeft"
+              text="voltar"
+              onClick={goBack}
+            />
+            <LinkComponent
+              text="ver no github"
+              href={postData.html_url}
+              target="_blank"
+            />
+          </header>
 
-      <h1>JavaScript data types and data structures</h1>
+          <h1>{postData.title}</h1>
 
-      <ul>
-        <li>
-          <FontAwesomeIcon icon={faGithub} />
-          Muglly
-        </li>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faGithub} />
+              {postData.user.login}
+            </li>
 
-        <li>
-          <FontAwesomeIcon icon={faCalendar} />
-          Há 1 dia
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faComment} /> 5 comentarios
-        </li>
-      </ul>
+            <li>
+              <FontAwesomeIcon icon={faCalendar} />
+              {formattedDate}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faComment} /> {postData.comments}{' '}
+              comentarios
+            </li>
+          </ul>
+        </>
+      )}
     </S.PostInfoContainer>
   )
 }
--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,10 +1,40 @@
+import { PostsProps } from '../Home'
+import { api } from '../../lib/axios'
+import { useParams } from 'react-router-dom'
+import { useCallback, useEffect, useState } from 'react'
 import { PostInfo } from './components/PostInfo'
+
 import * as S from './styles'
 
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const repoName = import.meta.env.VITE_GITHUB_REPONEME
+
 export function Post() {
+  const [postData, setPostData] = useState<PostsProps>({} as PostsProps)
+  const [isLoading, setIsloading] = useState(true)
+
+  const { id } = useParams()
+
+  const getPostDetails = useCallback(async () => {
+    try {
+      setIsloading(true)
+      const response = await api.get(
+        `/repos/${username}/${repoName}/issues/${id}`,
+      )
+
+      setPostData(response.data)
+    } finally {
+      setIsloading(false)
+    }
+  }, [postData])
+
+  useEffect(() => {
+    getPostDetails()
+  }, [])
+
   return (
     <>
-      <PostInfo />
+      <PostInfo isLoading={isLoading} postData={postData} />
     </>
   )
 }
